82ab9020d8c8c548d300e1419d55786c617a18da
abhil <abhilash@alraislabs.com>
2014-09-23 17:36:28 +0530

    cant display

---
+++ properties/platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,10 @@
+/home/alrais/properties/platforms/android/ant-build/classes.dex : \
+/home/alrais/properties/platforms/android/ant-build/classes/com/phonegap/helloworld/R$drawable.class \
+/home/alrais/properties/platforms/android/ant-build/classes/com/phonegap/helloworld/BuildConfig.class \
+/home/alrais/properties/platforms/android/ant-build/classes/com/phonegap/helloworld/R.class \
+/home/alrais/properties/platforms/android/ant-build/classes/com/phonegap/helloworld/R$attr.class \
+/home/alrais/properties/platforms/android/ant-build/classes/com/phonegap/helloworld/HelloWorld.class \
+/home/alrais/properties/platforms/android/ant-build/classes/com/phonegap/helloworld/R$string.class \
+/home/alrais/properties/platforms/android/ant-build/classes/com/phonegap/helloworld/R$xml.class \
+/home/alrais/properties/platforms/android/ant-build/dexedLibs/classes-19b6630010c88c24905395507aa850e3.jar \
+/home/alrais/properties/platforms/android/ant-build/dexedLibs/phonegap-1.0.0-8e03078c994979e5a2a4ad593d7bb122.jar \
